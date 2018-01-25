11115dca5c32d7b53ba4878eb735f0a3c9fed206
Flavio Carvalho <flavio@globalnetsis.com.br>
2018-01-25 17:38:07 -0200

    Ajustando os métodos do campaign

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -2,23 +2,20 @@ class CampaignsController < ApplicationController
   before_action :authenticate_user!
 
   before_action :set_campaign, only: [:show, :destroy, :update, :raffle]
-  #Checa se é proprietário da campanha
   before_action :is_owner?, only: [:show, :destroy, :update, :raffle]
 
   def show
   end
 
-  #lista todas as campanhas do usuário logado
   def index
     @campaigns = current_user.campaigns
   end
 
   def create
-    @campaign = Campaign.new(campaign_params)
+    @campaign = Campaign.new(user: current_user, title: 'Nova Campanha', description: 'Descreva sua campanha...')
 
     respond_to do |format|
       if @campaign.save
-        #Se salvar direcionar para a campanha
         format.html { redirect_to "/campaigns/#{@campaign.id}" }
       else
         format.html { redirect_to main_app.root_url, notice: @campaign.errors }
@@ -44,10 +41,8 @@ class CampaignsController < ApplicationController
     end
   end
 
-  #Sorteado
   def raffle
     respond_to do |format|
-      #Só vai sortear se status diferente de pendente
       if @campaign.status != "pending"
         format.json { render json: 'Já foi sorteada', status: :unprocessable_entity }
       elsif @campaign.members.count < 3
@@ -69,9 +64,7 @@ class CampaignsController < ApplicationController
     params.require(:campaign).permit(:title, :description, :event_date, :event_hour, :location).merge(user: current_user)
   end
 
-  #Método de auntenticação para verificar se é o dono da campanha
   def is_owner?
-    #Senão
     unless current_user == @campaign.user
       respond_to do |format|
         format.json { render json: false, status: :forbidden }
